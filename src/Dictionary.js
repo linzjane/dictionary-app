d161c127f862a69444453620e5b0048073ca86c4
linzjane <87429934+linzjane@users.noreply.github.com>
2021-10-25 16:24:29 +0100

    Added images

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,20 +1,29 @@
 import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
   let [results, setResults] = useState(null);
+  let [photos, setPhotos] = useState(null);
 
   function handleResponse(response) {
     setResults(response.data[0]);
   }
-
+  function handlePexelsResponse(response) {
+    setPhotos(response.data.photos);
+  }
   function search(event) {
     event.preventDefault();
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
+    let pexelsApiKey =
+      "563492ad6f91700001000001d41f7a55fc3b4e03b2366fd73781eebe";
+    let pexelsApiUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
   }
   function handlKeywordChange(event) {
     setKeyword(event.target.value);
@@ -34,6 +43,7 @@ export default function Dictionary() {
       </section>
       <br />
       <Results results={results} />
+      <Photos photos={photos} />
     </div>
   );
 }
